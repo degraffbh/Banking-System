5cb45d285b77399d99080be180e37a80cf5d9f75
degraffbh <benjamindegraff@gmail.com>
2025-04-15 18:05:09 -0500

    Deposit and Withdraw Functions Coded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import time
 
 connection = mysql.connector.connect(user = 'root', database = 'banking', password = 'Brook9!!!')
 cursor = connection.cursor()
@@ -6,6 +7,7 @@ testQuery = ('SELECT * FROM user')
 cursor.execute(testQuery)
 for item in cursor:
     print(item)
+cursor.close()
 
 def login_screen():
     print("\nBen's Banking System\nLogin Page\n")
@@ -28,40 +30,81 @@ def login_screen():
 
         sql = "INSERT INTO user (account_num, pin, balance, account_name) VALUES (%s, %s, %s, %s)"
         val = (account_number, pin, balance, name)
+        cursor = connection.cursor()
         cursor.execute(sql, val)
         connection.commit()
+        cursor.close()
 
         print(f"Ben's Banking account {account_number} created sucessfully!")
 
 def menu_page():
-    print("\nWhat would you like to do today?")
-    print("(1) Check Balance")
-    print("(2) Deposit")
-    print("(3) Withdraw")
-    print("(4) Create Account")
-    print("(5) Delete Account")
-    print("(6) Modify Account")
-    answer = int(input("> "))
+    isUsingApp = True
+    while isUsingApp == True:
+        print("\nWhat would you like to do today?")
+        print("(1) Check Balance")
+        print("(2) Deposit")
+        print("(3) Withdraw")
+        print("(4) Create Account")
+        print("(5) Delete Account")
+        print("(6) Modify Account")
+        print("(7) Exit")
+        answer = int(input("> "))
 
-    #Need to code all the actions 
-    match answer:
-        case 1:
-            print("\nCheck Balance")
-        case 2:
-            print("\nDeposit")
-        case 3:
-            print("\nWithdraw")
-        case 4:
-            print("\nCreate Account")
-        case 5:
-            print("\nDelete Account")
-        case 6:
-            print("\nModify Account")
-        case _:
-            print("\nInvalid command")
+        #Need to code all the actions 
+        match answer:
+            case 1:
+                getBalanceQuery = ('SELECT balance FROM user WHERE account_name = "Ben DeGraff"') #Need to add account_name varaible
+                cursor = connection.cursor()
+                cursor.execute(getBalanceQuery)
+                for item in cursor:
+                    print(f'\nYour current balance is ${str(item)[1:len(item)-3]}')
+                cursor.close()
+                time.sleep(1.5)
+            case 2:
+                new_balance = int(input("How much money would you like to deposit? "))
+                getBalanceQuery = ('SELECT balance FROM user WHERE account_name = "Ben DeGraff"') #Need to add account_name varaible
+                cursor = connection.cursor()
+                cursor.execute(getBalanceQuery)
+                for item in cursor:
+                    new_balance += int(str(item)[1:len(item)-3])
+                cursor.close()
+
+                updateBalanceQuery = ('UPDATE user SET balance = %s WHERE account_name = %s') 
+                val = (new_balance, "Ben DeGraff")
+                cursor = connection.cursor()
+                cursor.execute(updateBalanceQuery, val)
+                connection.commit()
+                cursor.close()
+                print("Balance Updated")
+            case 3:
+                new_balance = int(input("How much money would you like to withdraw? "))
+                getBalanceQuery = ('SELECT balance FROM user WHERE account_name = "Ben DeGraff"') #Need to add account_name varaible
+                cursor = connection.cursor()
+                cursor.execute(getBalanceQuery)
+                for item in cursor:
+                    new_balance = int(str(item)[1:len(item)-3]) - new_balance
+                cursor.close()
+
+                updateBalanceQuery = ('UPDATE user SET balance = %s WHERE account_name = %s') 
+                val = (new_balance, "Ben DeGraff")
+                cursor = connection.cursor()
+                cursor.execute(updateBalanceQuery, val)
+                connection.commit()
+                cursor.close()
+                print("Balance Updated")
+            case 4:
+                print("\nCreate Account")
+            case 5:
+                print("\nDelete Account")
+            case 6:
+                print("\nModify Account")
+            case 7:
+                print("\nHave a good rest of your day!")
+                isUsingApp = False
+            case _:
+                print("\nInvalid command")
 
 login_screen()
 menu_page()
 
-cursor.close()
 connection.close()
