a9cabe81c995f0833a273e4eb13792f06588b59b
degraffbh <benjamindegraff@gmail.com>
2025-04-22 16:29:09 -0500

    Added Sign In Feature

--- main.py
+++ main.py
@@ -9,15 +9,54 @@ for item in cursor:
     print(item)
 cursor.close()
 
+USER_ACCOUNT_NAME = None
+
 def login_screen():
     print("\nBen's Banking System\nLogin Page\n")
 
     login_or_new = input("Sign in (s) or make a new account (n)? ")
     if (login_or_new == "s"):
-        account_number = input("Please enter your account number: ")
-        pin = int(input("Please enter your pin: "))
+        isValid = False
+       
+        while isValid == False:
+            validNum = False
+            validPin = False
+            account_number = int(input("Please enter your account number: "))
+            pin = int(input("Please enter your pin: "))
 
-        #Add code to check if account number is in database and if pin matches
+            #Checking for valid account_num
+            checkAccountQuery = ('SELECT %s FROM user WHERE account_num = %s') 
+            val = (pin, account_number)
+            cursor = connection.cursor()
+            cursor.execute(checkAccountQuery, val)
+            for item in cursor:
+                validNum = True
+            cursor.close()
+
+            #Checking for valid pin
+            checkPinQuery = ('SELECT %s FROM user WHERE pin = %s') 
+            val = (account_number, pin)
+            cursorTwo = connection.cursor()
+            cursorTwo.execute(checkPinQuery, val)
+            for item in cursorTwo:
+                validPin = True
+            cursorTwo.close()
+
+            if validNum == True and validPin == True:
+                isValid = True
+                name = None
+                getNameQuery = ('SELECT account_name FROM user WHERE account_num = %s')
+                val = (account_number,)
+                cursorThree = connection.cursor()
+                cursorThree.execute(getNameQuery, val)
+                for item in cursorThree:
+                    USER_ACCOUNT_NAME = str(item)[2:len(item)-4]
+                    print(f'\nWelcome {str(item)[2:len(item)-4]}!')
+                cursorThree.close()
+            else:
+                print("Login information incorrect. Please try again.")
+            
+            
     else:
         name = input("What is your name? ")
         account_number = input("Please enter an account number: ")
@@ -35,6 +74,7 @@ def login_screen():
         connection.commit()
         cursor.close()
 
+        USER_ACCOUNT_NAME = name
         print(f"Ben's Banking account {account_number} created sucessfully!")
 
 def menu_page():
@@ -44,10 +84,9 @@ def menu_page():
         print("(1) Check Balance")
         print("(2) Deposit")
         print("(3) Withdraw")
-        print("(4) Create Account")
-        print("(5) Delete Account")
-        print("(6) Modify Account")
-        print("(7) Exit")
+        print("(4) Delete Account")
+        print("(5) Modify Account")
+        print("(6) Exit")
         answer = int(input("> "))
 
         #Need to code all the actions 
@@ -77,14 +116,20 @@ def menu_page():
                 cursor.close()
                 print("Balance Updated")
             case 3:
-                new_balance = int(input("How much money would you like to withdraw? "))
-                getBalanceQuery = ('SELECT balance FROM user WHERE account_name = "Ben DeGraff"') #Need to add account_name varaible
-                cursor = connection.cursor()
-                cursor.execute(getBalanceQuery)
-                for item in cursor:
-                    new_balance = int(str(item)[1:len(item)-3]) - new_balance
-                cursor.close()
-
+                isValid = False
+                while isValid == False:
+                    new_balance = int(input("How much money would you like to withdraw? "))
+                    getBalanceQuery = ('SELECT balance FROM user WHERE account_name = "Ben DeGraff"') #Need to add account_name varaible
+                    cursor = connection.cursor()
+                    cursor.execute(getBalanceQuery)
+                    for item in cursor:
+                        if int(str(item)[1:len(item)-3]) >= new_balance:
+                            new_balance = int(str(item)[1:len(item)-3]) - new_balance
+                            isValid = True
+                        else:
+                            print("You don't have that much money in your account. Please try again.")
+                    cursor.close()
+               
                 updateBalanceQuery = ('UPDATE user SET balance = %s WHERE account_name = %s') 
                 val = (new_balance, "Ben DeGraff")
                 cursor = connection.cursor()
@@ -93,12 +138,10 @@ def menu_page():
                 cursor.close()
                 print("Balance Updated")
             case 4:
-                print("\nCreate Account")
-            case 5:
                 print("\nDelete Account")
-            case 6:
+            case 5:
                 print("\nModify Account")
-            case 7:
+            case 6:
                 print("\nHave a good rest of your day!")
                 isUsingApp = False
             case _:
